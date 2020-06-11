a89260399e7145004959cb5c55f32479012dfc8d
Sayan Goswami <goswami.sayan47@gmail.com>
2020-06-11 22:26:12 +0530

    Add logging, check for change before reloading

--- hotreload_test.py
+++ hotreload_test.py
@@ -1,14 +1,18 @@
 #! /usr/bin/env python3
 
 import time
+import logging
 from hotreload import Loader
 
 
 if __name__ == "__main__":
-    loader = Loader("script.py")
+    logging.basicConfig(level=logging.INFO)
+    script = Loader("script.py")
 
     while True:
-        loader.f1()
-        loader.f2()
+        # Check if script has been modified since last poll.
+        if script.has_changed():
+            # Execute a function from script if it has been modified.
+            script.main()
 
         time.sleep(1)
