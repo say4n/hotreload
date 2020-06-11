914956775caf01c414bc45148a9f3913a153c672
Sayan Goswami <goswami.sayan47@gmail.com>
2020-06-11 22:25:36 +0530

    Add has_changed method, add logging

--- hotreload.py
+++ hotreload.py
@@ -5,7 +5,9 @@ import importlib
 import time
 import hashlib
 import threading
+import logging
 
+logger = logging.getLogger("hot_reload")
 
 class Monitor(threading.Thread):
     def __init__(self, loader, frequency = 1):
@@ -34,12 +36,27 @@ class Loader:
         self.module = importlib.import_module(self.__name)
         self.fingerprint = None
 
+        self.changed = False
+
         monitor = Monitor(self)
         monitor.start()
 
     def notify(self, fingerprint):
         self.fingerprint = fingerprint
-        self.module = importlib.reload(self.module)
+        try:
+            logger.info(f"Fingerprint changed to {fingerprint[:7]}, reloading.")
+            self.module = importlib.reload(self.module)
+            self.changed = True
+        except Exception as e:
+            logger.error(f"Reload failed. {e}")
+
+    def has_changed(self):
+        logger.info("Loader.has_changed called, self.changed is {self.changed}")
+        if self.changed:
+            self.changed = False
+            return True
+        else:
+            return False
 
     def __getattr__(self, attr):
         return getattr(self.module, attr)
