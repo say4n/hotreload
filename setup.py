1cd021702887346109650a2764d97433f18d9406
Sayan Goswami <goswami.sayan47@gmail.com>
2020-06-12 14:38:43 +0530

    Reorganize project

---
+++ setup.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+import setuptools
+import subprocess
+
+
+def get_git_revision_short_hash():
+    return subprocess.check_output(['git',
+                                    'rev-parse',
+                                    '--short',
+                                    'HEAD']).decode('ascii').strip()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = datetime.now().strftime("%Y.%m.%d") + "+" + get_git_revision_short_hash()
+
+setuptools.setup(
+    name="hotreload", # Replace with your own username
+    version=version,
+    author="Sayan Goswami",
+    author_email="me@sayan.page",
+    description="Run any arbitrary python script every time the code changes in the file.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/say4n/hotreload.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
